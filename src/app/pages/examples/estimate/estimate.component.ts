0c5687abbbdc52958dc552a9e94509670a30d1b3
David Knoernschild <david@seattleweb.com>
2023-03-04 09:55:56 -0800

    Save estimate

--- src/app/pages/examples/estimate/estimate.component.ts
+++ src/app/pages/examples/estimate/estimate.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { API } from 'aws-amplify';
-import { first } from "rxjs";
+import { OrdersService } from '../../../orders.service';
+import { Router } from "@angular/router"
 
 @Component({
   selector: "app-estimate",
@@ -11,13 +12,18 @@ import { first } from "rxjs";
 export class EstimateComponent implements OnInit {
 
   estimate = {
+    id: null,
     necessaryServices: [{name:'',price:null}],
     optionalServices: [{name:'',price:null}]
   };
 
+  order = null;
+
   constructor(
     private http: HttpClient,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private ordersService: OrdersService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -27,7 +33,7 @@ export class EstimateComponent implements OnInit {
       let id = params['id'];
       console.log('on init, id is', id);
 
-      // try this
+      this.estimate.id = id;
 
       const myInit = {
         headers: {}, // OPTIONAL
@@ -50,6 +56,14 @@ export class EstimateComponent implements OnInit {
           console.log('got error');
         });
     });
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      console.log("ngOnInit");
+      let id = params['id'];
+      this.ordersService.getOrder(id).subscribe((data) => {
+        this.order = data;
+      });
+    });
   }
 
   public addNecessaryService(): void {
@@ -70,5 +84,23 @@ export class EstimateComponent implements OnInit {
     const firstHalf = this.estimate.optionalServices.slice(0,i)
     const secondHalf = this.estimate.optionalServices.slice(i+1,this.estimate.optionalServices.length)
     this.estimate.optionalServices = firstHalf.concat(secondHalf);
+  }
+
+  public saveEstimate(): void {
+
+    const myInit = {
+      body: this.estimate, // replace this with attributes you need
+      headers: {} // OPTIONAL
+    };
+
+    API.post("estimates", "/estimates", myInit)
+  .then((response) => {
+    // Add your code here
+    console.log("estimate posted");
+    this.router.navigate(['examples/repairs'])
+  })
+  .catch((error) => {
+    console.log(error.response);
+  });
   }
 }
